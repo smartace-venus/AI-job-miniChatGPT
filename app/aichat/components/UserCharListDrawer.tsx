39909c8f3db8be9e18f5dfc5656b0ba3b0cdbc12
matiasgabriel1116 <matiasgrabrieloviedo@gmail.com>
2025-04-14 13:36:39 -0400

    [!] fixed: modified sort files as updated Date

--- app/aichat/components/UserCharListDrawer.tsx
+++ app/aichat/components/UserCharListDrawer.tsx
@@ -213,7 +213,7 @@ const CombinedDrawer: FC<CombinedDrawerProps> = ({
   const sortedUserFiles = useMemo(() => {
     return [...userFiles].sort(
       (a, b) =>
-        new Date(b.created_at).getTime() - new Date(a.created_at).getTime()
+        new Date(b.updated_at).getTime() - new Date(a.updated_at).getTime()
     );
   }, [userFiles]);
 
