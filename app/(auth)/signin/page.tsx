c58692ef1345db52f3eac6c14fd8d70967c22c0d
matiasgabriel1116 <matiasgrabrieloviedo@gmail.com>
2025-04-22 10:49:32 -0400

    [!] fixed: auto-redirect when a user signin by role

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -4,11 +4,26 @@ import SignInCard from './SignInCard';
 import Content from '../Content';
 import { redirect } from 'next/navigation';
 import { getSession } from '@/lib/server/supabase';
+import { createServerSupabaseClient } from '@/lib/server/server';
 
 export default async function AuthPage() {
   const session = await getSession();
   if (session) {
-    redirect('/');
+    // redirect('/');
+    const supabase = await createServerSupabaseClient();
+    const { data: userData } = await supabase
+      .from('users')
+      .select('role')
+      .eq('id', session.id)
+      .single();
+
+    console.log("@@@ userData?.role => ", userData?.role)
+
+    if (userData?.role === 'admin') {
+      redirect('/admin');
+    } else {
+      redirect('/aichat');
+    }
   }
 
   return (
